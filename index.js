8476885fa651399015a2811cf4fd1f7070edec93
M3r1 <gili.mery@gmail.com>
2023-02-04 21:16:37 +0200

    Add Dockerfile

--- index.js
+++ index.js
@@ -33,6 +33,5 @@ const server = http.createServer(function (request, response) {
 });
 
 const port = 3000;
-const host = "127.0.0.1";
-server.listen(port, host);
-console.log(`Listening at http://${host}:${port}`);
+server.listen(port);
+console.log(`Listening at 0.0.0.0:${port}`);
